fac4cf425dad3900fb15e4dfdc81eedf2ab73d64
Raymice <aymeric.zanirato@gmail.com>
2026-03-08 16:55:41 -0400

    feat: add unit tests

--- server/service_test.go
+++ server/service_test.go
@@ -187,3 +187,280 @@ func TestIsUUIDFormtatted(t *testing.T) {
 		})
 	}
 }
+
+func TestServerService_ReconcilePlayStatusWithAllMovies(t *testing.T) {
+	tests := []struct {
+		name           string // description of this test case
+		client         *jellyfinClients.Client
+		allMovies      []apiModels.MovieLightAPI
+		userSeenMovies map[string][]apiModels.MovieLightAPI
+		users          []apiModels.UserAPI
+		want           []serverModels.MovieLightStatusDTO
+		wantErr        bool
+	}{
+		{
+			name:   "No movies",
+			client: &jellyfinClients.Client{},
+			want:   []serverModels.MovieLightStatusDTO(nil),
+		},
+		{
+			name:   "One movie, one user, seen",
+			client: &jellyfinClients.Client{},
+			allMovies: []apiModels.MovieLightAPI{
+				{
+					MovieXtLightAPI: apiModels.MovieXtLightAPI{
+						ID:   "1",
+						Name: "movie1",
+					},
+				},
+			},
+			userSeenMovies: map[string][]apiModels.MovieLightAPI{
+				"user1": {
+					{
+						MovieXtLightAPI: apiModels.MovieXtLightAPI{
+							ID:   "1",
+							Name: "movie1",
+						},
+					},
+				},
+			},
+			users: []apiModels.UserAPI{
+				{
+
+					ID:   "user1",
+					Name: "User One",
+				},
+			},
+			want: []serverModels.MovieLightStatusDTO{
+				{
+					MovieLightAPI: apiModels.MovieLightAPI{
+						MovieXtLightAPI: apiModels.MovieXtLightAPI{
+							ID:   "1",
+							Name: "movie1",
+						},
+					},
+					UserPlayStatuses: []serverModels.UserPlayStatusDTO{
+						{
+							UserID:    "user1",
+							UserName:  "User One",
+							Played:    true,
+							PlayCount: 0,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:   "One movie, one user, not seen",
+			client: &jellyfinClients.Client{},
+			allMovies: []apiModels.MovieLightAPI{
+				{
+					MovieXtLightAPI: apiModels.MovieXtLightAPI{
+						ID:   "1",
+						Name: "movie1",
+					},
+				},
+			},
+			userSeenMovies: map[string][]apiModels.MovieLightAPI{
+				"user1": {},
+			},
+			users: []apiModels.UserAPI{
+				{
+
+					ID:   "user1",
+					Name: "User One",
+				},
+			},
+			want: []serverModels.MovieLightStatusDTO{
+				{
+					MovieLightAPI: apiModels.MovieLightAPI{
+						MovieXtLightAPI: apiModels.MovieXtLightAPI{
+							ID:   "1",
+							Name: "movie1",
+						},
+					},
+					UserPlayStatuses: []serverModels.UserPlayStatusDTO{
+						{
+							UserID:    "user1",
+							UserName:  "User One",
+							Played:    false,
+							PlayCount: 0,
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:   "Multiple movies, multiple users",
+			client: &jellyfinClients.Client{},
+			allMovies: []apiModels.MovieLightAPI{
+				{
+					MovieXtLightAPI: apiModels.MovieXtLightAPI{
+						ID:   "1",
+						Name: "movie1",
+					},
+				},
+				{
+					MovieXtLightAPI: apiModels.MovieXtLightAPI{
+						ID:   "2",
+						Name: "movie2",
+					},
+				},
+			},
+			userSeenMovies: map[string][]apiModels.MovieLightAPI{
+				"user1": {
+					{
+						MovieXtLightAPI: apiModels.MovieXtLightAPI{
+							ID:   "1",
+							Name: "movie1",
+						},
+					},
+				},
+				"user2": {
+					{
+						MovieXtLightAPI: apiModels.MovieXtLightAPI{
+							ID:   "2",
+							Name: "movie2",
+						},
+					},
+				},
+			},
+			users: []apiModels.UserAPI{
+				{
+
+					ID:   "user1",
+					Name: "User One",
+				},
+				{
+
+					ID:   "user2",
+					Name: "User Two",
+				},
+			},
+			want: []serverModels.MovieLightStatusDTO{
+				{
+					MovieLightAPI: apiModels.MovieLightAPI{
+						MovieXtLightAPI: apiModels.MovieXtLightAPI{
+							ID:   "1",
+							Name: "movie1",
+						},
+					},
+					UserPlayStatuses: []serverModels.UserPlayStatusDTO{
+						{
+							UserID:    "user1",
+							UserName:  "User One",
+							Played:    true,
+							PlayCount: 0,
+						},
+						{
+							UserID:    "user2",
+							UserName:  "User Two",
+							Played:    false,
+							PlayCount: 0,
+						},
+					},
+				},
+				{
+					MovieLightAPI: apiModels.MovieLightAPI{
+						MovieXtLightAPI: apiModels.MovieXtLightAPI{
+							ID:   "2",
+							Name: "movie2",
+						},
+					},
+					UserPlayStatuses: []serverModels.UserPlayStatusDTO{
+						{
+							UserID:    "user1",
+							UserName:  "User One",
+							Played:    false,
+							PlayCount: 0,
+						},
+						{
+							UserID:    "user2",
+							UserName:  "User Two",
+							Played:    true,
+							PlayCount: 0,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:   "Movie seen by multiple users",
+			client: &jellyfinClients.Client{},
+			allMovies: []apiModels.MovieLightAPI{
+				{
+					MovieXtLightAPI: apiModels.MovieXtLightAPI{
+						ID:   "1",
+						Name: "movie1",
+					},
+				},
+			},
+			userSeenMovies: map[string][]apiModels.MovieLightAPI{
+				"user1": {
+					{
+						MovieXtLightAPI: apiModels.MovieXtLightAPI{
+							ID:   "1",
+							Name: "movie1",
+						},
+					},
+				},
+				"user2": {
+					{
+						MovieXtLightAPI: apiModels.MovieXtLightAPI{
+							ID:   "1",
+							Name: "movie1",
+						},
+					},
+				},
+			},
+			users: []apiModels.UserAPI{
+				{
+
+					ID:   "user1",
+					Name: "User One",
+				},
+				{
+
+					ID:   "user2",
+					Name: "User Two",
+				},
+			},
+			want: []serverModels.MovieLightStatusDTO{
+				{
+					MovieLightAPI: apiModels.MovieLightAPI{
+						MovieXtLightAPI: apiModels.MovieXtLightAPI{
+							ID:   "1",
+							Name: "movie1",
+						},
+					},
+					UserPlayStatuses: []serverModels.UserPlayStatusDTO{
+						{
+							UserID:    "user1",
+							UserName:  "User One",
+							Played:    true,
+							PlayCount: 0,
+						},
+						{
+							UserID:    "user2",
+							UserName:  "User Two",
+							Played:    true,
+							PlayCount: 0,
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := server.NewService(tt.client)
+			got, gotErr := s.ReconcilePlayStatusWithAllMovies(tt.allMovies, tt.userSeenMovies, tt.users)
+			if gotErr != nil {
+				assert.Nil(t, tt.wantErr, "Expected no error but got: %v", gotErr)
+			}
+			assert.NotNil(t, tt.wantErr, "Expected an error but got nil")
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
